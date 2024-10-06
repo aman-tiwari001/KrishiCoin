107bcbd0b03ea99565906e636f028e510eeaba79
Shobhit Tiwari <122107079+Shobhit141141@users.noreply.github.com>
2024-10-07 00:34:08 +0530

    Update top padding in Home component

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,7 +4,7 @@ import RightCards from "../components/RightCards";
 
 export const Home = () => {
   return (
-    <div className="p-4 pt-[60px]">
+    <div className="p-4 pt-[75px]">
       <Banner />
       <div className="flex flex-col md:flex-row md:gap-4">
         <LeftCards />
