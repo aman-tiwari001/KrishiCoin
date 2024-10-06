1ce4d306e58bcaf07f505702f6731591a632d30c
anshd15 <anshdoshi.d16@gmail.com>
2024-10-07 00:09:04 +0530

    Landing Page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,30 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, useLocation } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar';
 import { Home } from './pages/Home';
+import LandingPage from './pages/LandingPage';
 
 function App() {
+	const location = useLocation();
+
+	const shouldShowNavbar = location.pathname !== '/';
+
 	return (
 		<>
-			<BrowserRouter>
-				<Navbar />
-				<Routes>
-					<Route path='/' element={<Home />} />
-				</Routes>
-			</BrowserRouter>
+			{shouldShowNavbar && <Navbar />}
+			
+			<Routes>
+				<Route path='/home' element={<Home />} />
+				<Route path='/' element={<LandingPage />} />
+			</Routes>
 		</>
 	);
 }
 
-export default App;
+export default function MainApp() {
+	return (
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	);
+}
