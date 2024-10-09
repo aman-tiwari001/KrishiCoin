bcb90a00da288c03f35ad5d58bdda2d0666d41b2
anshd15 <anshdoshi.d16@gmail.com>
2024-10-10 03:14:42 +0530

    Selling Page

--- client/src/components/LeftCards.jsx
+++ client/src/components/LeftCards.jsx
@@ -3,7 +3,11 @@ function LeftCards() {
     <div className="w-full md:w-[40%] h-[50vh] flex flex-col-reverse gap-2 py-4">
       {/* Top Card */}
       <div className=" w-full h-1/2 bg-white rounded-2xl cards-bg shadow-lg ">
-        <div className="w-[100%] h-[100%] bg-gradient-to-b from-slate-600 to-black opacity-[0.8] rounded-2xl flex items-center justify-center "><h1 className="text-4xl text-[#e0fce7] font-semibold">User Dashboard</h1></div>
+        <div className="w-[100%] h-[100%] bg-gradient-to-b from-[#283618] to-black opacity-[0.8] rounded-2xl flex items-center justify-center ">
+          <h1 className="text-4xl text-[#e0fce7] font-semibold">
+            User Dashboard
+          </h1>
+        </div>
       </div>
 
       {/* 2 small cards */}
@@ -20,12 +24,16 @@ function LeftCards() {
           </button>
         </div>
 
-        <div className="w-1/2 h-full rounded-2xl bg-white golden-border  p-6 flex flex-col items-center justify-center shadow-lg">
-          <h2 className="text-black text-2xl font-semibold mb-4">Add Funds</h2>
+        <div className="w-1/2 h-full rounded-2xl bg-white bg-gradient-to-bl from-[#7C5D25] to-[#C9A53D]  flex flex-col items-center justify-center shadow-lg">
+          <div className="w-[96%] h-[96%] rounded-2xl bg-white m-2  p-6 flex flex-col items-center justify-center ">
+            <h2 className="text-black text-xl font-semibold mb-4">
+              Add Funds to your wallet
+            </h2>
 
-          <button className="bg-black text-white font-bold py-2 px-8 rounded-full shadow transition duration-300 hover:bg-gray-800">
-            Add Funds
-          </button>
+            <button className="bg-gradient-to-bl from-[#7C5D25] to-[#C9A53D] text-white font-bold py-2 px-8 rounded-full shadow transition duration-300 hover:bg-gray-800">
+              Add Funds
+            </button>
+          </div>
         </div>
       </div>
     </div>
--- client/src/components/RightCards.jsx
+++ client/src/components/RightCards.jsx
@@ -3,7 +3,7 @@ function RightCards() {
     <div className="w-full md:w-[60%] h-[50vh] flex flex-col gap-2 md:py-4">
       {/* Top Card */}
       <div className="w-full h-1/2 bg-[#19747E] cards2-bg rounded-2xl  ">
-        <div className="w-[100%] h-[100%] bg-gradient-to-br from-[#1c6350] to-[#000000] flex flex-col items-center justify-center shadow-lg opacity-[0.8] px-4 py-6 rounded-2xl  ">
+        <div className="w-[100%] h-[100%] bg-gradient-to-br from-[#20705b] to-[#012513] flex flex-col items-center justify-center shadow-lg opacity-[0.8] px-4 py-6 rounded-2xl  ">
           <h2 className="text-[#e0fce7] text-2xl font-semibold mb-4">
             Join Communities
           </h2>
@@ -20,17 +20,27 @@ function RightCards() {
 
       {/* 3 small cards */}
       <div className="flex w-full h-1/2 gap-2">
-        <div className="w-1/2 h-2/3 md:h-full bg-[#A9D6E5] rounded-2xl p-4 flex flex-col justify-evenly shadow-lg">
-          <h2 className="text-gray-800 text-xl font-bold">P2P Lending</h2>
-          <p className="text-gray-800 text-[10px] md:text-[15px]">
-            Connect directly with lenders. Get better rates and flexible terms.
-          </p>
+        <div className="w-2/4 h-2/3 md:h-full bg-[#A9D6E5] rounded-2xl  flex flex-col justify-evenly shadow-lg">
+          <div className="w-[100%] h-[100%] z-50 bg-gradient-to-br from-[#A4C8E1] to-[#5a8cbc] flex flex-col items-center justify-center shadow-lg opacity-[0.8] px-4 py-6 rounded-2xl  ">
+            <h2 className="text-white text-xl font-bold">Sell Your Crops</h2>
+            <p className="text-white text-[10px] md:text-[15px]">
+              Connect directly with lenders. Get better rates and flexible
+              terms.
+            </p>
+          </div>
         </div>
-        <div className="w-1/2 h-2/3 md:h-full bg-[#A9D6E5] rounded-2xl p-4 flex flex-col justify-evenly shadow-lg">
-          <h2 className="text-gray-800 text-xl font-bold">Get quick Loan</h2>
-          <p className="text-gray-800 text-[10px] md:text-[15px]">
-            Apply for a loan quickly and easily. Fast approval and funding.
-          </p>
+        <div className="w-1/4 h-2/3 md:h-full bg-[#A9D6E5] rounded-2xl  flex flex-col justify-evenly shadow-lg">
+          <div className="w-[100%] h-[100%] z-50 bg-gradient-to-br from-[#71aad2] to-[#203673] flex flex-col items-center justify-center shadow-lg opacity-[0.8] px-4 py-6 rounded-2xl  ">
+            <h2 className="text-[#e0fce7] text-xl font-bold">Sell Crops</h2>
+            
+          </div>
+        </div>
+        <div className="w-1/4 h-2/3 md:h-full  rounded-2xl cards3-bg flex flex-col items-center justify-center shadow-lg">
+          <div className="w-[100%] h-[100%] z-50 bg-black flex flex-col items-center justify-center shadow-lg opacity-[0.7] px-4 py-6 rounded-2xl  ">
+          <h2 className="text-white top-4 text-xl font-bold">Marketplace</h2>
+            
+          </div>
+
         </div>
       </div>
     </div>
