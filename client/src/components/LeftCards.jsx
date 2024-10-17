84aedabed51e90175535319a38fc3e635f02b7dd
Aman Tiwari <amananjalitiwari2007@gmail.com>
2024-10-17 19:16:51 +0530

    feat: integrated coinbase onramp so that user can add funds to their wallet

--- client/src/components/LeftCards.jsx
+++ client/src/components/LeftCards.jsx
@@ -1,47 +1,57 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
+
+const openCBOnrampPopup = () => {
+	const url = `https://pay.coinbase.com/buy/select-asset?appId=${
+		import.meta.env.VITE_COINBASE_PROJECT_ID
+	}&addresses={"${localStorage.getItem('base-wallet-address')}":["ethereum"]}`;
+	window.open(url, 'popup', 'width=500,height=570');
+};
 
 function LeftCards() {
-  return (
-    <div className="w-full md:w-[40%] h-[50vh] flex flex-col-reverse gap-2 py-4">
-      {/* Top Card */}
-      <div className=" w-full h-1/2 bg-white rounded-2xl cards-bg shadow-lg ">
-        <div className="w-[100%] h-[100%] bg-gradient-to-b from-[#283618] to-black opacity-[0.8] rounded-2xl flex items-center justify-center ">
-          <h1 className="text-4xl text-[#e0fce7] font-semibold">
-            User Dashboard
-          </h1>
-        </div>
-      </div>
+	return (
+		<div className='w-full md:w-[40%] h-[50vh] flex flex-col-reverse gap-2 py-4'>
+			{/* Top Card */}
+			<div className=' w-full h-1/2 bg-white rounded-2xl cards-bg shadow-lg '>
+				<div className='w-[100%] h-[100%] bg-gradient-to-b from-[#283618] to-black opacity-[0.8] rounded-2xl flex items-center justify-center '>
+					<h1 className='text-4xl text-[#e0fce7] font-semibold'>
+						User Dashboard
+					</h1>
+				</div>
+			</div>
 
-      {/* 2 small cards */}
-      <div className="flex w-full h-1/2 gap-2">
-        <>
-          <Link
-            to="/donate"
-            className="w-1/2 h-full bg-[#283618] rounded-2xl px-10 max-md:px-4 py-6 flex flex-col items-start justify-center shadow-lg"
-          >
-            <h2 className="text-[#e0fce7] text-[20px] md:text-xl font-semibold mb-4">
-              Make a Donation
-            </h2>
-            <p className="text-[#e0fce7] mb-4 text-[15px] md:text-[13px]">
-              Be the one to make the change in people&apos;s lives
-            </p>
-          </Link>
-        </>
+			{/* 2 small cards */}
+			<div className='flex w-full h-1/2 gap-2'>
+				<>
+					<Link
+						to='/donate'
+						className='w-1/2 h-full bg-[#283618] rounded-2xl px-10 max-md:px-4 py-6 flex flex-col items-start justify-center shadow-lg'
+					>
+						<h2 className='text-[#e0fce7] text-[20px] md:text-xl font-semibold mb-4'>
+							Make a Donation
+						</h2>
+						<p className='text-[#e0fce7] mb-4 text-[15px] md:text-[13px]'>
+							Be the one to make the change in people&apos;s lives
+						</p>
+					</Link>
+				</>
 
-        <div className="w-1/2 h-full rounded-2xl bg-white bg-gradient-to-bl from-[#7C5D25] to-[#C9A53D]  flex flex-col items-center justify-center shadow-lg">
-          <div className="w-[96%] h-[96%] rounded-2xl bg-white m-2  p-6 flex flex-col items-center justify-center ">
-            <h2 className="text-black text-xl font-semibold mb-4">
-              Add Funds to your wallet
-            </h2>
+				<div className='w-1/2 h-full rounded-2xl bg-white bg-gradient-to-bl from-[#7C5D25] to-[#C9A53D]  flex flex-col items-center justify-center shadow-lg'>
+					<div className='w-[96%] h-[96%] rounded-2xl bg-white m-2  p-6 flex flex-col items-center justify-center '>
+						<h2 className='text-black text-xl font-semibold mb-4'>
+							Add Funds to your wallet
+						</h2>
 
-            <button className="bg-gradient-to-bl btn border-0 from-[#7C5D25] to-[#C9A53D] text-white font-bold py-2 px-8 rounded-full shadow transition duration-300 hover:bg-gray-800">
-              Add Funds
-            </button>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+						<button
+							onClick={openCBOnrampPopup}
+							className='bg-gradient-to-bl btn border-0 from-[#7C5D25] to-[#C9A53D] text-white font-bold py-2 px-8 rounded-full shadow transition duration-300 hover:bg-gray-800'
+						>
+							Add Funds
+						</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default LeftCards;
