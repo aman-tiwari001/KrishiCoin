eaab42e9d6e5232a2894282dcad221c2f075dfdd
Shobhit Tiwari <122107079+Shobhit141141@users.noreply.github.com>
2024-10-21 00:40:04 +0530

    Refactor: Update import statement for Providers in main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,9 +3,10 @@ import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import '@coinbase/onchainkit/styles.css';
-import { Providers } from './utils/provider.jsx';
+
 import { getConfig } from './utils/wagmi.js';
 import { cookieToInitialState } from 'wagmi';
+import { Providers } from './utils/Provider.jsx';
 
 const initialState = cookieToInitialState(getConfig());
 
