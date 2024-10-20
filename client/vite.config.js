aef5ed216d942c31194834b07a2a50feb4543a33
Shobhit Tiwari <122107079+Shobhit141141@users.noreply.github.com>
2024-10-21 00:54:56 +0530

    Refactor: Update Vite configuration to support top-level await and target ES2022

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    target: "es2022"
+  },
+  esbuild: {
+    supported: {
+      'top-level-await': true
+    },
+  },
   plugins: [react()],
 })
